42d44b0fd47c2b3857593cd47224518dd7f7cc0b
CurryBoard1 <25420045@uttcampus.edu.mx>
2026-08-13 01:17:18 -0600

    switch y fin

--- Ejercicio/src/Main.java
+++ Ejercicio/src/Main.java
@@ -1,13 +1,139 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import modelos.Alumno;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+
+            System.out.println("========== MENU ==========");
+            System.out.println("1. Registrar alumno");
+            System.out.println("2. Mostrar alumnos");
+            System.out.println("3. Actualizar alumno");
+            System.out.println("4. Eliminar alumno");
+            System.out.println("5. Contar hombres y mujeres");
+            System.out.println("6. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+        switch (opcion) {
+
+            case 1:
+
+                Alumno alumno = new Alumno();
+
+                System.out.print("Matrícula: ");
+                alumno.setMatricula(sc.nextLine());
+
+                System.out.print("Nombre: ");
+                alumno.setNombre(sc.nextLine());
+
+                System.out.print("Edad: ");
+                alumno.setEdad(sc.nextInt());
+                sc.nextLine();
+
+                System.out.print("Sexo (M/F): ");
+                alumno.setSexo(sc.nextLine());
+
+                System.out.print("Correo: ");
+                alumno.setCorreo(sc.nextLine());
+
+                alumno.save();
+
+                break;
+
+            case 2:
+
+                ArrayList<Alumno> lista = Alumno.getAll();
+
+                if (lista.isEmpty()) {
+
+                    System.out.println("No hay alumnos registrados.");
+
+                } else {
+
+                    for (Alumno a : lista) {
+
+                        System.out.println("--------------------------------");
+                        System.out.println("ID: " + a.getId());
+                        System.out.println("Matrícula: " + a.getMatricula());
+                        System.out.println("Nombre: " + a.getNombre());
+                        System.out.println("Edad: " + a.getEdad());
+                        System.out.println("Sexo: " + a.getSexo());
+                        System.out.println("Correo: " + a.getCorreo());
+
+                    }
+
+                }
+
+                break;
+
+            case 3:
+
+                System.out.print("Ingrese la matrícula del alumno: ");
+                String matricula = sc.nextLine();
+
+                Alumno actualizar = Alumno.findByMatricula(matricula);
+
+                if (actualizar == null) {
+
+                    System.out.println("Alumno no encontrado.");
+
+                } else {
+
+                    System.out.print("Nuevo nombre: ");
+                    actualizar.setNombre(sc.nextLine());
+
+                    System.out.print("Nueva edad: ");
+                    actualizar.setEdad(sc.nextInt());
+                    sc.nextLine();
+
+                    System.out.print("Nuevo sexo: ");
+                    actualizar.setSexo(sc.nextLine());
+
+                    System.out.print("Nuevo correo: ");
+                    actualizar.setCorreo(sc.nextLine());
+
+                    actualizar.update();
+
+                }
+
+                break;
+
+            case 4:
+
+                System.out.print("Ingrese la matrícula del alumno: ");
+                String eliminar = sc.nextLine();
+
+                Alumno.deleteByMatricula(eliminar);
+
+                break;
+
+            case 5:
+
+                Alumno.countBySexo();
+
+                break;
+
+            case 6:
+
+                System.out.println("Programa finalizado.");
+
+                break;
+
+            default:
+
+                System.out.println("Opción no válida.");
+
+        }
+
+        } while (opcion != 6);
+
     }
 }
