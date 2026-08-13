c9f6bb40ce7f3e5c307525350ff0bc8cb2d9930a
CurryBoard1 <25420045@uttcampus.edu.mx>
2026-08-13 00:50:32 -0600

    getters y setters

--- Ejercicio/src/modelos/Alumno.java
+++ Ejercicio/src/modelos/Alumno.java
@@ -7,4 +7,63 @@ public class Alumno {
     private int edad;
     private String sexo;
     private String correo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Alumno(int id, String matricula, String correo, String sexo, int edad, String nombre) {
+        this.id = id;
+        this.matricula = matricula;
+        this.correo = correo;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.nombre = nombre;
+
+
+    }
 }
