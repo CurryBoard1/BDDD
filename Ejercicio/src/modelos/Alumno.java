1330bffdfe4c59e55956e79478a64cbb47652b97
CurryBoard1 <25420045@uttcampus.edu.mx>
2026-08-13 00:48:06 -0600

    clase alumno con sus atributos

---
+++ Ejercicio/src/modelos/Alumno.java
@@ -0,0 +1,10 @@
+package modelos;
+
+public class Alumno {
+    private int id;
+    private String matricula;
+    private String nombre;
+    private int edad;
+    private String sexo;
+    private String correo;
+}
