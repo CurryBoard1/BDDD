601f6b202ecd99e9b026114cd1333719a221ec8a
CurryBoard1 <25420045@uttcampus.edu.mx>
2026-08-13 00:54:02 -0600

    metodo de registrar y buscar por matricula

--- Ejercicio/src/modelos/Alumno.java
+++ Ejercicio/src/modelos/Alumno.java
@@ -1,4 +1,6 @@
 package modelos;
+import database.Conexion;
+
 
 public class Alumno {
     private int id;
@@ -66,4 +68,70 @@ public class Alumno {
 
 
     }
+
+
+    // REGISTRAR ALUMNO
+
+
+    public void save() {
+
+        String sql = "INSERT INTO alumno(matricula,nombre,edad,sexo,correo) VALUES(?,?,?,?,?)";
+
+        try (Connection con = Conexion.conectar();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, matricula);
+            ps.setString(2, nombre);
+            ps.setInt(3, edad);
+            ps.setString(4, sexo);
+            ps.setString(5, correo);
+
+            ps.executeUpdate();
+
+            System.out.println("Alumno registrado correctamente.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // BUSCAR POR MATRICULA
+
+
+    public static Alumno findByMatricula(String matriculaBuscar) {
+
+        Alumno alumno = null;
+
+        String sql = "SELECT * FROM alumno WHERE matricula=?";
+
+        try (Connection con = Conexion.conectar();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, matriculaBuscar);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                alumno = new Alumno();
+
+                alumno.setId(rs.getInt("id"));
+                alumno.setMatricula(rs.getString("matricula"));
+                alumno.setNombre(rs.getString("nombre"));
+                alumno.setEdad(rs.getInt("edad"));
+                alumno.setSexo(rs.getString("sexo"));
+                alumno.setCorreo(rs.getString("correo"));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return alumno;
+
+    }
+
+}
 }
